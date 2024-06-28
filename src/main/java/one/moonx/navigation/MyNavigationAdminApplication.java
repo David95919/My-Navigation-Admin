ffbaf8e1661f745976a8d54c1aedde77faddfef5
David95919 <150328571+David95919@users.noreply.github.com>
2024-06-28 22:39:47 +0800

    github actions test

--- src/main/java/one/moonx/navigation/MyNavigationAdminApplication.java
+++ src/main/java/one/moonx/navigation/MyNavigationAdminApplication.java
@@ -4,6 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//SpringBoot
 @SpringBootApplication
 @MapperScan("one.moonx.navigation.mapper")
 public class MyNavigationAdminApplication {
