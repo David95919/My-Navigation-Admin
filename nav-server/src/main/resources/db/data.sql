c2af61140a92301ca68780c81208201148ca36a4
David95919 <150328571+David95919@users.noreply.github.com>
2024-07-21 17:12:10 +0800

    fix: init user

--- nav-server/src/main/resources/db/data.sql
+++ nav-server/src/main/resources/db/data.sql
@@ -1,4 +1,7 @@
-insert into t_user(id, username, password, create_time, update_time)
-values (1, 'admin', 'e10adc3949ba59abbe56e057f20f883e', now(), now());
+INSERT INTO t_user(id, username, password, create_time, update_time)
+SELECT 1, 'admin', 'e10adc3949ba59abbe56e057f20f883e', NOW(), NOW()
+WHERE NOT EXISTS (SELECT 1
+                  FROM t_user
+                  WHERE id = 1 OR username = 'admin');
 
 # admin 123456
