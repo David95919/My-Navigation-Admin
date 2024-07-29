4f93383a6ec7de78d7a9b5db746c4a174ce0bc3c
David95919 <150328571+David95919@users.noreply.github.com>
2024-07-29 19:09:58 +0800

    feat: userCategoryController

---
+++ nav-server/src/main/java/one/moonx/navigation/controller/user/CategoryController.java
@@ -0,0 +1,33 @@
+package one.moonx.navigation.controller.user;
+
+import one.moonx.navigation.base.Result;
+import one.moonx.navigation.constant.MessageConstant;
+import one.moonx.navigation.convert.CategoryConvert;
+import one.moonx.navigation.pojo.vo.CategoryVO;
+import one.moonx.navigation.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController("userCategoryController")
+@RequestMapping("/user/category")
+public class CategoryController {
+    @Autowired
+    private CategoryService categoryService;
+    @Autowired
+    private CategoryConvert categoryConvert;
+
+    /**
+     * 获取类别
+     *
+     * @return {@link Result }<{@link List }<{@link CategoryVO }>>
+     */
+    @GetMapping
+    public Result<List<CategoryVO>> getCategory() {
+        List<CategoryVO> categoryVOS = categoryConvert.convertVO(categoryService.list());
+        return Result.success.msgAndData(MessageConstant.GET_SUCCESS, categoryVOS);
+    }
+}
